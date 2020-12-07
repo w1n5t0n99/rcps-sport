9a0f7418606d5051b429ade159cfb7e61ec5b55d
relam <relam@rcschools.us>
2020-12-07 16:17:50 -0500

    add db test tables

--- rcps-sport.py
+++ rcps-sport.py
@@ -19,10 +19,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+#db.create_all()
 bootstrap = Bootstrap(app)
 
-comments = ['comment 0', 'comment 1', 'comment 2', 'comment 3']
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
 
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
