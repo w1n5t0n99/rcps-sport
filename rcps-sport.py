676488b6dcac38e2fbe3bc85588965e36e680b3e
relam <relam@rcschools.us>
2020-12-07 11:28:00 -0500

    add error templates

--- rcps-sport.py
+++ rcps-sport.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+comments = ['comment 0', 'comment 1', 'comment 2', 'comment 3']
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', comments=comments)
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
