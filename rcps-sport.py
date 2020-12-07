30fe55813eae7a1175f2113a21e6efc33d69173a
relam <relam@rcschools.us>
2020-12-07 13:48:21 -0500

    add wtf forms

--- rcps-sport.py
+++ rcps-sport.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'app secret key'
 bootstrap = Bootstrap(app)
 
 comments = ['comment 0', 'comment 1', 'comment 2', 'comment 3']
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', comments=comments)
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
