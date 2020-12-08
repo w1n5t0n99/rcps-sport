36b7c2377c806ffa852a9f84d5c03c55cbaa722f
relam <relam@rcschools.us>
2020-12-08 11:14:09 -0500

    add database test entries

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -22,23 +23,24 @@ db = SQLAlchemy(app)
 #db.create_all()
 bootstrap = Bootstrap(app)
 
-class Role(db.Model):
-    __tablename__ = 'roles'
+class Sport(db.Model):
+    __tablename__ = 'sport'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    games = db.relationship('Game', backref='sport')
 
     def __repr__(self):
-        return '<Role %r>' % self.name
+        return '<Sport %r>' % self.name
 
-class User(db.Model):
-    __tablename__ = 'users'
+class Game(db.Model):
+    __tablename__ = 'game'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    name = db.Column(db.String(64), unique=True, index=True)
+    date = db.Column(db.Date, nullable=False)
+    sport_id = db.Column(db.Integer, db.ForeignKey('sport.id'))
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Game %r>' % self.name
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
